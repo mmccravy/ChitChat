dcf6e16a9a10a989bb24a3a012e25354a280a40d
mmccravy <matthewmccravy@gmail.com>
2023-11-22 03:16:08 -0600

    Update chattyControlMessage.cs

--- Controller/chattyControlMessage.cs
+++ Controller/chattyControlMessage.cs
@@ -1,1 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Chatty.Properties
+{
+    [Route("api/")]
+    [ApiController]
+    public class chattyControlMessage : ControllerBase
+    {
+        private readonly ChatService _chatService;
+        public chattyControllMessage(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] superData data)
+        {
+            var result = _chatService.OnChat(data.Data.From, data.Data.Body);
+            Console.WriteLine(JsonSerializer.Serialize(data));
+            return Ok(data);
+        }
+    }
+}
 
